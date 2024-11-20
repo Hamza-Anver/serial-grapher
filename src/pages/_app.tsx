b9b81beb2796a7521f33735a2e3fd818783ba8d5
Hamza Anver <hamzaanver1234@gmail.com>
2024-11-20 18:41:24 -0500

    shadcn initialized sidebar test and theme toggle

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,25 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 
+import { SidebarProvider, SidebarTrigger } from "@/components/ui/sidebar";
+import { AppSidebar } from "@/components/app-sidebar";
+import { ThemeProvider } from "@/components/theme-provider";
+
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <ThemeProvider
+      attribute="class"
+      defaultTheme="system"
+      enableSystem
+      disableTransitionOnChange
+    >
+      <SidebarProvider>
+        <AppSidebar />
+        <main>
+          <SidebarTrigger />
+          <Component {...pageProps} />
+        </main>
+      </SidebarProvider>
+    </ThemeProvider>
+  );
 }
